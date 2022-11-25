eb48eb66d4907d66637a9c220cfeb7071ce57d02
Adam <19adamchristie96@gmail.com>
2022-11-25 17:01:01 +0000

    Create subtract function that takes multiple parameters and subtracts from first number entered as argument

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ function add (...arguments){
 }
 
 console.log(add(2, 2, 2, 2));
+
+function subtract (...arguments){
+    let total = arguments[0];
+    for(let i = 1; i < arguments.length; i++){
+        total -= arguments[i];
+    }
+    return total;
+}
+
+console.log(subtract(10, 2, 4));
