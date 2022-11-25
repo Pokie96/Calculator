e10bfb10be279fd8507ffcc59141b87e7004af42
Adam <19adamchristie96@gmail.com>
2022-11-25 16:52:48 +0000

    Change the sum function so that it can accept multiple arguments and return the sum of all of them

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-function add (a, b){
-    return a + b;
+function add (...arguments){
+    let total = 0;
+    for(let i = 0; i < arguments.length; i++){
+        total += arguments[i];
+    }
+    return total;
 }
 
-console.log(add(2, 2));
+console.log(add(2, 2, 2, 2));
